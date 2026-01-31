5add6c13d2708eb6a378d7bd4f91d8a4232c37eb
tiffanyytu <tiffanytu2024@gmail.com>
2026-01-31 14:42:22 -0500

    Connect to Supabase and render captions list

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -3,4 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
+// ADDING THIS LINE TO CHECK IF KEYS ARE FOUND:
+console.log("Supabase URL Check:", supabaseUrl);
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
